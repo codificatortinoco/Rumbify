7e435d16f5ce8d31f4ebc5eb6440d7a2f2d1d918
Brandon <martinezbran0221@gmail.com>
2025-10-30 17:35:01 -0500

    Fix (supabase) keep fixing deployment error

--- server/services/socket.service.js
+++ server/services/socket.service.js
@@ -1,34 +1,22 @@
-//const { Server } = require("socket.io");
-const supabase = require("./supabase.service");
-const { channel } = require("./supabase.service");
+// const { Server } = require("socket.io");
+const supabase = require("./.supabase.service");
 
-let io;
+let channel;
 
 const initSocketInstance = (httpServer) => {
- // io = new Server(httpServer, {
-  //   path: "/real-time",
-  //   cors: {
-  //     origin: "*",
-  //   },
-  // });
-
   channel = supabase.channel("realtime-events");
 };
 
 const emitEvent = async (eventName, data) => {
- // if (!io) {
- //   throw new Error("Socket.io instance is not initialized");
- // }
- // io.emit(eventName, data);
- if (!channel) {
-   throw new Error("Supabase channel is not initialized");
- }
- const resp = await channel.send({
-    type: "broadcast",  
+  if (!channel) {
+    throw new Error("Supabase channel is not initialized");
+  }
+  const resp = await channel.send({
+    type: "broadcast",
     event: eventName,
     payload: data,
   });
-  
+
   return resp;
 };
 
